3a1bf43fcdd51a4a7919d7bc4419e53d4b144a92
JuneXi <31943251+JuneXi@users.noreply.github.com>
2017-09-13 19:35:45 -0700

    Update Singleton.java

--- src/Fortinet/Singleton.java
+++ src/Fortinet/Singleton.java
@@ -1,1 +1,13 @@
+public class Singleton {
+	private static Singleton singleton = null;
+	private Singleton(){}
+	public static Singleton getInstance(){
+		if(singleton == null){
+			synchronized(Singleton.class){
+				singleton = new Singleton();
+			}
+		}
+		return singleton;
+	}
 
+}
