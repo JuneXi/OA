a49a94b8899a12e62515ad9ed8f80d7d08806330
JuneXi <31943251+JuneXi@users.noreply.github.com>
2017-09-14 21:47:21 -0700

    Update Q2MultiDimensionArraySum.java
    added some comment and fixed a bug

--- src/Fortinet/Q2MultiDimensionArraySum.java
+++ src/Fortinet/Q2MultiDimensionArraySum.java
@@ -16,17 +16,22 @@ public static Long getValue(int... indexOfDimension) {
     public static Long sum(MultiDimensionArray mArray, int[] lengthOfDeminsion) { 
    // Time complexity:  O(n)
    // Space complexity: O(1)
+    	
     	long sum = 0;
+    	//Initialize an array to store the indexOfDimension
     	int[] iod = new int[lengthOfDeminsion.length];
+   	
+    	/*It's easy to imagine that we should use a loop that has variable number of levels (not sure if it's allowed in java) 
+    	But we can calculate the multiplication of the domain from each level 
+    	Then map the index back using only one variable.*/
     	int mul = 1;
     	for(int i = 0; i < lengthOfDeminsion.length; i++){
     		mul = mul * lengthOfDeminsion[i];
     	}
-    	
     	for(int i = 0; i < mul; i++){
     		int temp = mul;
     		for(int j = 0; j < lengthOfDeminsion.length; j++){
-    			iod[j] = i * temp / lengthOfDeminsion[j] % lengthOfDeminsion[j];
+    			iod[j] = i * lengthOfDeminsion[j] / temp  % lengthOfDeminsion[j];
     			temp = temp / lengthOfDeminsion[j];
     		}
     		sum = sum + getValue(iod);
